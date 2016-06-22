19d3a4290848adf60cd1fe9e5b7319f9d377ce1a
brarnaudovski <brankoarnaudovski@yahoo.com>
2016-06-22 22:08:46 +0200

    model: Create Broadband model

---
+++ app/models/broadband.rb
@@ -0,0 +1,2 @@
+class Broadband < ActiveRecord::Base
+end
---
+++ test/models/broadband_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BroadbandTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
