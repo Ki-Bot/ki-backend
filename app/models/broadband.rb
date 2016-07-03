7d163bbe77556f7cb21096eba89cef1e48b8044b
brarnaudovski <brankoarnaudovski@yahoo.com>
2016-07-03 20:57:26 +0200

    add custom KI geo data for the location

--- app/models/broadband.rb
+++ app/models/broadband.rb
@@ -2,7 +2,12 @@ class Broadband < ActiveRecord::Base
   include AlgoliaSearch
 
   algoliasearch do
-    attribute :address, :city, :state_code, :id, :latitude, :longitude
+    attribute :address, :city, :state_code, :id
+    attribute :_geoloc do
+      { lat: self.latitude, lng: self.longitude }
+    end
+
+
     attributesToIndex ['address', 'city', 'state_code']
   end
 end
