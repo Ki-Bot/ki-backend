fdeb6a7fbe96d0132547432ebe5de3cee8d0e4ee
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-14 16:20:31 +0200

    Add shallow resource nesting

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, param: :uid do
-    resources :locations, only: :index, controller: :points do
+    resources :locations, only: :index, controller: :points, shallow: true do
       post 'favorite', to: 'points#create'
       delete 'favorite', to: 'points#destroy'
     end
