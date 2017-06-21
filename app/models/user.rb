26ef972ef0fa04f88ccf853be3716f7c5206b6a2
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-21 14:41:00 +0200

    Add auth token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token 32
+    end while self.class.exists?(auth_token: auth_token)
+  end
+
 end
