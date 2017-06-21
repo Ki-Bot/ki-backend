2d78d2930ad59471ef69f644d3f83f73c600f2a4
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-21 14:17:32 +0200

    Add devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  root to: "home#index"
+
+  devise_for :users
 
   namespace :api, defaults: {fromat: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
