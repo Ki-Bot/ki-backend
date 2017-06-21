06e42747622e00240b0285bc8d77a6823524db09
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-21 14:36:21 +0200

    Add spec request and response helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'shoulda_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -55,4 +56,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Request::HeadersHelpers, type: :controller
+  config.include Response::JsonHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
