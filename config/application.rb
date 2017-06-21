4952c98d69e4ba8cd38508581adeee72b7fe7fcd
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-21 14:11:10 +0200

    Add rspec and api constraints

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module KIProjectApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.stylesheets = false
+      g.javascripts = false
+    end
+
+    config.autoload_paths += %W(\#{config.root}/lib)
   end
 end
