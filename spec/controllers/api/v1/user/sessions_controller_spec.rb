7419f9b7ca57cd15a6b73fb736b262b1215c8c31
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-22 11:42:49 +0200

    Refactored sessions specs

--- spec/controllers/api/v1/user/sessions_controller_spec.rb
+++ spec/controllers/api/v1/user/sessions_controller_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Users::SessionsController, type: :controller do
+  let(:user) {FactoryGirl.create :user}
 
   describe 'POST #create' do
-    before(:each) do
-      @user = FactoryGirl.create :user
-    end
-
     context 'when the credentials are correct' do
       before(:each) do
-        credentials = {email: @user.email, password: '12345678'}
+        credentials = {email: user.email, password: '12345678'}
         post :create, params: {user: credentials}
       end
 
@@ -18,8 +15,8 @@ RSpec.describe Api::V1::Users::SessionsController, type: :controller do
       end
 
       it 'returns the users record corresponding to the given credentials' do
-        @user.reload
-        expect(json_response[:auth_token]).to eql @user.auth_token
+        user.reload
+        expect(json_response[:auth_token]).to eql user.auth_token
       end
 
       it {is_expected.to respond_with :success}
@@ -27,7 +24,7 @@ RSpec.describe Api::V1::Users::SessionsController, type: :controller do
 
     context 'when the credentials are incorrect' do
       before(:each) do
-        credentials = {email: @user.email, password: 'invalidpassword'}
+        credentials = {email: user.email, password: 'invalidpassword'}
         post :create, params: {user: credentials}
       end
 
@@ -40,20 +37,17 @@ RSpec.describe Api::V1::Users::SessionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    before(:each) do
-      @user = FactoryGirl.create :user
-      @old_token = @user.auth_token
-    end
+    let!(:old_token) {user.auth_token}
 
     context 'when users has a valid authentication token' do
       before(:each) do
-        api_authorization_header @user.auth_token
+        api_authorization_header user.auth_token
         delete :destroy
       end
 
       it "users's token should change" do
-        @user.reload
-        expect(@user.auth_token).not_to eql @old_token
+        user.reload
+        expect(user.auth_token).not_to eql old_token
       end
 
       it {is_expected.to respond_with :no_content}
@@ -66,8 +60,8 @@ RSpec.describe Api::V1::Users::SessionsController, type: :controller do
       end
 
       it "users's token shouldn't change" do
-        @user.reload
-        expect(@user.auth_token).to eql @old_token
+        user.reload
+        expect(user.auth_token).to eql old_token
       end
 
       it {is_expected.to respond_with :unauthorized}
