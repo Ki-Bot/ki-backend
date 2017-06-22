1e6cc0038b6dd111ff995909f31a0f8baeee7f9f
Jure Cindro <jure.cindro@poviolabs.com>
2017-06-22 10:54:29 +0200

    Added additional user controller specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  let(:user) {FactoryGirl.create :user}
 
   describe 'GET #me' do
-    let(:user) { FactoryGirl.create :user }
-    before(:each) do
-      api_authorization_header user.auth_token
-      get :me
+    context 'user is authenticated' do
+      before(:each) do
+        api_authorization_header user.auth_token
+        get :me
+      end
+      it 'returns the information about a user on a hash' do
+        expect(json_response[:email]).to eql user.email
+      end
+
+      it "doesn't contain auth token" do
+        expect(json_response).not_to include :auth_token
+      end
+
+      it {is_expected.to respond_with 200}
     end
 
-    it 'returns the information about a user on a hash' do
-      expect(json_response[:email]).to eql user.email
-    end
+    context 'user is not authenticated' do
+      before(:each) do
+        get :me
+      end
 
-    it {is_expected.to respond_with 200}
+      it {is_expected.to respond_with 401}
+    end
   end
 
 end
