8fbd3e707faf217ec3c44287396e8a5431932aaf
agonq <agon.qurdina@gmail.com>
2017-06-29 21:51:42 +0200

    Broadband CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :allow_iframe, only: :test_facebook
 
   def test_facebook
     render 'test_facebook'
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
