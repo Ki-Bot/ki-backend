55c3a081e5226c32ef44fd1fae2dda8ad010c73d
agonq <agon.qurdina@gmail.com>
2017-07-03 02:54:13 +0200

    Filter anchors, Parse categories, Twitter Login

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@
 # puts AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 
-BroadbandType.destroy_all
-BroadbandType.create!(name: 'City and Village halls')
-BroadbandType.create!(name: 'Park Districts')
-BroadbandType.create!(name: 'Hospitals')
-BroadbandType.create!(name: 'Police Departments')
-BroadbandType.create!(name: 'Fire Departments')
-BroadbandType.create!(name: 'Schools')
-BroadbandType.create!(name: 'Other')
-
 Broadband.without_auto_index do
   Broadband.destroy_all
+  BroadbandType.destroy_all
+  BroadbandType.create!(name: 'City and Village halls')
+  BroadbandType.create!(name: 'Park Districts')
+  BroadbandType.create!(name: 'Hospitals')
+  BroadbandType.create!(name: 'Police Departments')
+  BroadbandType.create!(name: 'Fire Departments')
+  BroadbandType.create!(name: 'Schools')
+  BroadbandType.create!(name: 'Other')
+
   array = Array(0..370)
   idx = 0
   array.each do |i|
