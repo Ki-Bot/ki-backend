5dd8c29f46674db061109c47615ac9dc8c065294
agonq <agon.qurdina@gmail.com>
2017-07-08 11:53:25 +0200

    Fixes & changes

--- db/schema.rb
+++ db/schema.rb
@@ -113,33 +113,3 @@ ActiveRecord::Schema.define(version: 20170702204038) do
     t.index ["user_id"], name: "index_user_broadbands_on_user_id"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "roles", default: [], array: true
-    t.string "email", default: ""
-    t.string "encrypted_password", default: ""
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "auth_token"
-    t.string "uid"
-    t.string "provider"
-    t.string "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
-  end
-
-  add_foreign_key "broadbands", "broadband_types"
-  add_foreign_key "opening_hours", "broadbands"
-  add_foreign_key "user_broadbands", "broadbands"
-  add_foreign_key "user_broadbands", "users"
-end
