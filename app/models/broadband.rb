c8cc28a1a72a000c074b3b1e12435f96f3010c89
agonq <agon.qurdina@gmail.com>
2017-07-09 17:52:01 +0200

    New version

--- app/models/broadband.rb
+++ app/models/broadband.rb
@@ -41,7 +41,7 @@ class Broadband < ApplicationRecord
     end
     hash[:filters] = filter_text unless filter_text.nil?
     json = Broadband.raw_search(nil, hash)
-    json['hits'].map { |hit| { id: hit['objectID'].to_i, address: hit['address'], _geoloc: hit['_geoloc'], type: hit['type'] } }
+    json['hits'].map { |hit| { id: hit['objectID'].to_i, address: hit['address'], anchorname: hit['anchorname'], _geoloc: hit['_geoloc'], type: hit['type'] } }
     # json = index.search({
     #     filters: '(type:Hospitals)'
     # })
