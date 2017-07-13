a5417f488594162f4d83cc00eba09cd7ec603c9b
agonq <agon.qurdina@gmail.com>
2017-07-13 02:40:27 +0200

    Fixes & changes.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :email, :name, :created_at, :updated_at, :roles, :provider, :oauth_token
+  attributes :email, :name, :created_at, :updated_at, :roles, :provider, :oauth_token, :profile_picture
 end
