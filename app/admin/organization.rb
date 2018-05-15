c6129079e3a74e74e54071ec9d98d106a73e2427
Muhammad Mohsin Ali <mohammadmohsin@gmail.com>
2018-05-15 23:23:32 +0500

    Organization tab added in active admin dashboard

---
+++ app/admin/organization.rb
@@ -0,0 +1,41 @@
+ActiveAdmin.register Organization do
+  actions :index
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  menu priority: 8
+
+  # permit_params :provider, :uid, :name, roles: []
+  # permit_params :id, :name, :email
+
+  index do
+    id_column
+
+    column :name
+    column :email
+    column :manager_name
+    column :phone_no
+    column :address
+    column :user_id
+    column :access_code
+    # column :name
+    # column :roles
+    # column :oauth_expires_at
+    # column :oauth_token
+    # column :created_at
+    # column :updated_at
+
+  end
+
+  filter :id
+end
