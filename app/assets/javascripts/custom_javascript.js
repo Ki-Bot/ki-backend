0ca7af2832dd5cf9ac154e2744fa3fe1791d4c32
tahir-s <abumutahira@gmail.com>
2018-10-16 21:15:40 +0500

    sign out issue

--- app/assets/javascripts/custom_javascript.js
+++ app/assets/javascripts/custom_javascript.js
@@ -2,54 +2,54 @@
  * Created by agonq on 6/24/17.
  */
 
-$(function(){
-    // if (jQuery.ui) {
-    //     alert('UI loaded');
-    // }
-    $('[data-method="get"]').removeAttr('data-method');
-    // $('[data-method="post"]').removeAttr('data-method');
+// $(function(){
+//     // if (jQuery.ui) {
+//     //     alert('UI loaded');
+//     // }
+//     $('[data-method="get"]').removeAttr('data-method');
+//     // $('[data-method="post"]').removeAttr('data-method');
 
-    //For delete links
-    $('a[data-method="delete"]').each(function(index, link){
-        $(link).on('click', function(){
-            if(confirm('Are you sure you want to delete?')){
-                $.ajax({
-                    url: $(this).attr('href'),
-                    method: 'DELETE',
-                    success: function(data){
-                        if(data.url){
-                            window.location = data.url;
-                        } else if(data.message){
-                            alert(data.message);
-                        }
-                    },
-                    error: function(err1, err2){
-                        alert(JSON.stringify(err1) + '\n' + JSON.stringify(err2));
-                    }
-                });
-                return false;
-            }
-        });
-    });
-    $('[data-method="delete"]').removeAttr('data-method').removeAttr('data-confirm');
+//     //For delete links
+//     $('a[data-method="delete"]').each(function(index, link){
+//         $(link).on('click', function(){
+//             if(confirm('Are you sure you want to delete?')){
+//                 $.ajax({
+//                     url: $(this).attr('href'),
+//                     method: 'DELETE',
+//                     success: function(data){
+//                         if(data.url){
+//                             window.location = data.url;
+//                         } else if(data.message){
+//                             alert(data.message);
+//                         }
+//                     },
+//                     error: function(err1, err2){
+//                         alert(JSON.stringify(err1) + '\n' + JSON.stringify(err2));
+//                     }
+//                 });
+//                 return false;
+//             }
+//         });
+//     });
+//     $('[data-method="delete"]').removeAttr('data-method').removeAttr('data-confirm');
 
-    //For update forms
-    $('form:has(input[name="_method"][value="patch"])').on('submit', function(){
-        $.ajax({
-            url: $(this).attr('action'),
-            method: 'PUT',
-            data: $(this).serialize(),
-            success: function(data){
-                if(data.url){
-                    window.location = data.url;
-                } else if(data.message){
-                    alert(data.message);
-                }
-            },
-            error: function(err1, err2){
-                alert(JSON.stringify(err1) + '\n' + JSON.stringify(err2));
-            }
-        });
-        return false;
-    });
-});
+//     //For update forms
+//     $('form:has(input[name="_method"][value="patch"])').on('submit', function(){
+//         $.ajax({
+//             url: $(this).attr('action'),
+//             method: 'PUT',
+//             data: $(this).serialize(),
+//             success: function(data){
+//                 if(data.url){
+//                     window.location = data.url;
+//                 } else if(data.message){
+//                     alert(data.message);
+//                 }
+//             },
+//             error: function(err1, err2){
+//                 alert(JSON.stringify(err1) + '\n' + JSON.stringify(err2));
+//             }
+//         });
+//         return false;
+//     });
+// });
